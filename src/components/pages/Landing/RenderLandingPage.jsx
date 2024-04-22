e14fd3439aa75e853e425838f1044ec53f36df71
Yingying Wang <yxw241@gmail.com>
2024-04-21 21:51:18 -0400

    -m 'first commit'

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -32,7 +32,54 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <div className="image1">
+          <img src={GrantRatesByOfficeImg} alt="Search Grant Rates By Office" />
+          <p
+            style={{
+              fontWeight: 'bold',
+              color: 'black',
+              fontSize: '18px',
+              fontFamily: 'Times New Roman, serif',
+              textAlign: 'center',
+            }}
+          >
+            Search Grant Rates By Office
+          </p>
+        </div>
+        <div className="image2">
+          <img
+            src={GrantRatesByNationalityImg}
+            alt="Search Grant Rates By Nationality"
+          />
+          <p
+            style={{
+              fontWeight: 'bold',
+              color: 'black',
+              fontSize: '18px',
+              fontFamily: 'Times New Roman, serif',
+              textAlign: 'center',
+            }}
+          >
+            Search Grant Rates By Nationality
+          </p>
+        </div>
+        <div className="image3">
+          <img src={GrantRatesOverTimeImg} alt="Search Grant Rates Over Time" />
+          <p
+            style={{
+              fontWeight: 'bold',
+              color: 'black',
+              fontSize: '18px',
+              fontFamily: 'Times New Roman, serif',
+              textAlign: 'auto',
+            }}
+          >
+            Search Grant Rates Over Time
+          </p>
+        </div>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -40,6 +87,12 @@ function RenderLandingPage(props) {
           onClick={() => history.push('/graphs')}
         >
           View the Data
+        </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+        >
+          Download the Data
         </Button>
       </div>
 
@@ -61,10 +114,109 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
-        <p onClick={() => scrollToTop()} className="back-to-top">
-          Back To Top ^
-        </p>
+        <div className="bottom-section">
+          <h1
+            className="bottom-text"
+            style={{
+              fontWeight: 'bold',
+              color: 'black',
+              fontSize: '46px',
+              fontFamily: 'Times New Roman, serif',
+              textAlign: 'center',
+              marginTop: '7rem',
+            }}
+          >
+            System Disparity Insights
+          </h1>
+
+          <div className="stats-container">
+            <div className="stat-box">
+              <h2
+                style={{
+                  fontWeight: 'bold',
+                  color: 'black',
+                  fontSize: '42px',
+                  fontFamily: 'Times New Roman, serif',
+                }}
+              >
+                36%
+              </h2>
+              <p
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '18px',
+                  fontFamily: 'Times New Roman, serif',
+                }}
+              >
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+            <div className="stat-box">
+              <h2
+                style={{
+                  fontWeight: 'bold',
+                  color: 'black',
+                  fontSize: '42px',
+                  fontFamily: 'Times New Roman, serif',
+                }}
+              >
+                5%
+              </h2>
+              <p
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '18px',
+                  fontFamily: 'Times New Roman, serif',
+                }}
+              >
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </p>
+            </div>
+            <div className="stat-box">
+              <h2
+                style={{
+                  fontWeight: 'bold',
+                  color: 'black',
+                  fontSize: '42px',
+                  fontFamily: 'Times New Roman, serif',
+                }}
+              >
+                6x Lower
+              </h2>
+              <p
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: '18px',
+                  fontFamily: 'Times New Roman, serif',
+                }}
+              >
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+
+          <div className="read-more-button-container">
+            <Button
+              type="default"
+              style={{
+                backgroundColor: '#404C4A',
+                color: '#FFFFFF',
+                marginTop: 80,
+              }}
+            >
+              Read More
+            </Button>
+          </div>
+
+          <p onClick={() => scrollToTop()} className="back-to-top">
+            Back To Top ^
+          </p>
+        </div>
       </div>
     </div>
   );
