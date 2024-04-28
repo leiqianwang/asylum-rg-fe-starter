f269cc543c24176acf609064ce423facc36894bc
Yingying Wang <yxw241@gmail.com>
2024-04-28 12:12:03 -0400

    3rd commit

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import CitizenshipMapAll from './Graphs/CitizenshipMapAll';
@@ -13,16 +13,22 @@ import { resetVisualizationQuery } from '../../../state/actionCreators';
 import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
+import { setVisualizationData } from '../../../state/actionCreators';
 
 const { background_color } = colors;
 
 function GraphWrapper(props) {
   const { set_view, dispatch } = props;
   let { office, view } = useParams();
+  //const { office, view: initialView } = useParams();
+  //useEffect(() => {
   if (!view) {
     set_view('time-series');
     view = 'time-series';
   }
+  // }, [view, set_view])
+
+  // Determine the component to render based on the view and office
   let map_to_render;
   if (!office) {
     switch (view) {
@@ -50,8 +56,69 @@ function GraphWrapper(props) {
         break;
     }
   }
+
   function updateStateWithNewData(years, view, office, stateSettingCallback) {
-    /*
+    let baseURL = 'https://hrf-asylum-be-b.herokuapp.com/cases';
+    let endpoint = '';
+
+    // Determin which endpoint to use based on the view
+    if (view === 'time-series') {
+      endpoint = '/fiscalSummary';
+    } else {
+      endpoint = '/citizenshipSummary';
+    }
+
+    //Setup parameters for the API call
+    const params = {
+      from: years[0],
+      to: years[1],
+      ...(office && { office }), // If an office is specified, add it to the parameters
+    };
+
+    //Perform the local API call by using callback function stateSettingCallback
+    axios
+      .get(`${baseURL}${endpoint}`, { params })
+      .then(response => {
+        //Pass fetched data to the callback to update state accordingly
+        stateSettingCallback(response.data);
+      })
+      .catch(err => {
+        console.error('Failed to fetch data:', err);
+      });
+  }
+
+  // useEffect(() => {
+  //   if(!years || !views) return;
+  //   updateStateWithNewData(years, view, office, handleFetchedData);
+
+  // }, [years, view, office, handleFetchedData]);
+
+  //     useEffect(() => {
+  //       if (!view) return; // Ensure we have a valid view before fetching data
+
+  //       const baseURL = "https://hrf-asylum-be-b.herokuapp.com/cases";
+  //       const endpoint = view === 'citizenship' ? '/citizenshipSummary' : '/fiscalSummary';
+  //       const params = {
+  //           from: 2015,  // Start year, presumably needs to be dynamic based on user input
+  //           to: 2022,    // End year, same as above
+  //           ...(office && { office })  // Include office in the parameters if specified
+  //       };
+
+  //       axios.get(`${baseURL}${endpoint}`, { params })
+  //           .then(response => {
+  //               // Dispatch the fetched data to the Redux store
+  //               dispatch(setVisualizationData(view, office, response.data));
+  //           })
+  //           .catch(error => {
+  //               console.error('Failed to fetch data:', error);
+  //           });
+
+  //   }, [dispatch, view, office]);  // Dependencies for useEffect
+
+  //   return null;  // This component does not render anything itself
+  // }
+
+  /*
           _                                                                             _
         |                                                                                 |
         |   Example request for once the `/summary` endpoint is up and running:           |
@@ -73,42 +140,44 @@ function GraphWrapper(props) {
     
     */
 
-    if (office === 'all' || !office) {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-            office: office,
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    }
-  }
+  //   if (office === 'all' || !office) {
+  //     axios
+  //       .get(process.env.REACT_APP_API_URI, {
+  //         // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+  //         params: {
+  //           from: years[0],
+  //           to: years[1],
+  //         },
+  //       })
+  //       .then(result => {
+  //         stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+  //       })
+  //       .catch(err => {
+  //         console.error(err);
+  //       });
+  //   } else {
+  //     axios
+  //       .get(process.env.REACT_APP_API_URI, {
+  //         // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+  //         params: {
+  //           from: years[0],
+  //           to: years[1],
+  //           office: office,
+  //         },
+  //       })
+  //       .then(result => {
+  //         stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+  //       })
+  //       .catch(err => {
+  //         console.error(err);
+  //       });
+  //   }
+  // }
+
   const clearQuery = (view, office) => {
     dispatch(resetVisualizationQuery(view, office));
   };
+
   return (
     <div
       className="map-wrapper-container"
